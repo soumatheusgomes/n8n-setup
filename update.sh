0cefaeb34c1c06b38b274b1f6018ce439eabc9ab
Matheus Gomes <gomes7296@gmail.com>
2022-11-14 15:27:51 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt update
 sudo apt upgrade -y
-sudo apt autoremove
-sudo apt autoclean
+sudo apt autoremove -y
+sudo apt autoclean -y
 
 docker pull n8nio/n8n
 sudo docker-compose stop
